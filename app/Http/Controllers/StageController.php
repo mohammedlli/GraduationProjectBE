3e3d4dc43f19ebe5855889f2c38a1dc5a85160a3
azuemployeemohammed1 <mohammed.ahmed7468@gmail.com>
2026-03-20 17:55:11 +0300

    handle stage route

--- app/Http/Controllers/StageController.php
+++ app/Http/Controllers/StageController.php
@@ -2,9 +2,22 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Stage;
 use Illuminate\Http\Request;
 
 class StageController extends Controller
 {
     //
+
+      public function show(string $id){
+        $stage = Stage::find($id);
+        return response()->json($stage, 200);
+    }
+
+       public function getAll(){
+        $stage = Stage::all();
+        return response()->json($stage , 200);
+    }
+
+    
 }
